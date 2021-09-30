98db63ecab41768f17b0c3cf93514a5c2a247b8f
adam <82415896+adamfil@users.noreply.github.com>
2021-09-30 10:05:49 -0400

    Create vars.py
    defining global vars. api token is redacted, to obtain token follow https://www.youtube.com/watch?v=ixbhdFToEPI

---
+++ vars.py
@@ -0,0 +1,29 @@
+import helper
+import visualizations.geospatialvisualization
+import visualizations.timeseriesvisualization
+import pandas as pd
+import numpy as np
+import datetime
+
+valid_set = ['vhi', 'prismc-tmax-daily', 'prismc-tmin-daily', 'prismc-precip-daily', 'rtma_dew_point-hourly',
+             'rtma_pcp-hourly', 'rtma_temp-hourly', 'rtma_wind_u-hourly', 'rtma_wind_v-hourly', 'cpcc_precip_us-daily',
+             'cpcc_precip_global-daily', 'cpcc_temp_max-daily', 'cpcc_temp_min-daily', 'chirpsc_final_05-daily',
+             'chirpsc_final_25-daily',
+             'chirpsc_prelim_05-daily', 'era5_land_2m_temp-hourly', 'era5_land_precip-hourly',
+             'era5_land_surface_solar_radiation_downwards-hourly',
+             'era5_land_snowfall-hourly', 'era5_land_wind_u-hourly', 'era5_land_wind_v-hourly',
+             'era5_surface_runoff-hourly', 'era5_wind_100m_u-hourly',
+             'era5_wind_100m_v-hourly', 'era5_volumetric_soil_water_layer_1-hourly']
+
+VALID_DASHSET = []
+
+for set in valid_set:
+    VALID_DASHSET.append({'label': set, 'value': set})
+
+
+
+
+
+TOKEN = 'REDACTED'
+
+
