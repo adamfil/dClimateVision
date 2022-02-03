ae4f70aa5b8a66656cfdba321cfd49df68f2be70
adam <82415896+adamfil@users.noreply.github.com>
2022-02-03 15:51:39 -0500

    Update vars.py
    add valid lists for supporting classes.py

--- vars.py
+++ vars.py
@@ -1,5 +1,4 @@
 TOKEN = 'REDACTED'
-
 state_codes = {
   "10": "Delaware", "12": "Florida", "13": "Georgia", "15": "Hawaii", "16": "Idaho", "17": "Illinois", "18": "Indiana",
   "19": "Iowa", "20": "Kansas", "21": "Kentucky", "22": "Louisiana", "23": "Maine", "24": "Maryland",
@@ -20,9 +19,12 @@ all_dataset_types = ['Grid File Dataset History', 'GFS Forecasts', 'GHCN Dataset
 
 VALID_DATASET_TYPES = []
 
+VALID_DATASET_LIST = []
+
 for set in all_dataset_types:
     if set in ['Grid File Dataset History', 'CME Station History', 'Dutch Station History']:
         VALID_DATASET_TYPES.append({'label': set, 'value': set})
+        VALID_DATASET_LIST.append(set)
 #usda rma codes currently left off valid dataset types
 
 #'Grid File Dataset History', \
@@ -38,13 +40,11 @@ valid_gridfile_set = ['vhi', 'prismc-tmax-daily', 'prismc-tmin-daily', 'prismc-p
              'era5_wind_100m_v-hourly', 'era5_volumetric_soil_water_layer_1-hourly']
 
 VALID_GRIDFILE_DASHSET = []
+VALID_GRIDFILE_LIST = []
 
 for set in valid_gridfile_set:
-    if 'hourly' not in set:
-        VALID_GRIDFILE_DASHSET.append({'label': set, 'value': set})
-    else:
-        VALID_GRIDFILE_DASHSET.append({'label': set, 'value': set})
-
+    VALID_GRIDFILE_DASHSET.append({'label': set, 'value': set})
+    VALID_GRIDFILE_LIST.append(set)
 
 #'GFS Forecasts',\
 #need 1. dataset, 2. lat/long
@@ -81,32 +81,32 @@ for variable in valid_ghcn_variables:
 
 #'CME Station History',
 #need 1. station id and 2. weather variable
-valid_cme_stations = ['03927', '13874', '14732', '14922', '23169', '23232', '24229',
+VALID_CME_STAT_LIST = ['03927', '13874', '14732', '14922', '23169', '23232', '24229',
                       '93814', '94846', '47662', '03772', '06240']
 
 VALID_CME_STATIONS = []
-for station in valid_cme_stations:
+for station in VALID_CME_STAT_LIST:
     VALID_CME_STATIONS.append({'label': station, 'value': station})
 
-valid_cme_variables = ['TMAX', 'TMIN', 'TAVG']
+VALID_CME_VAR_LIST = ['TMAX', 'TMIN', 'TAVG']
 
 VALID_CME_VARIABLES = []
-for variable in valid_cme_variables:
+for variable in VALID_CME_VAR_LIST:
     VALID_CME_VARIABLES.append({'label': variable, 'value': variable})
 #'Dutch Station History',\
 #need 1. station id and 2. weather variable
-valid_dutch_stations = [209, 210, 215, 225, 229, 235, 240, 242, 248, 249, 251, 257, 258, 260, 265, 267, 269, 270, 273,
+VALID_DUTCH_STAT_LIST = [209, 210, 215, 225, 229, 235, 240, 242, 248, 249, 251, 257, 258, 260, 265, 267, 269, 270, 273,
                         275, 277, 278, 279, 280, 283, 285, 286, 290, 308, 310, 311, 312, 313, 315, 316, 319, 323, 324,
                         330, 331, 340, 343, 344, 348, 350, 356, 370, 375, 377, 380, 391]
 
 VALID_DUTCH_STATIONS = []
-for station in valid_dutch_stations:
+for station in VALID_DUTCH_STAT_LIST:
     VALID_DUTCH_STATIONS.append({'label': str(station), 'value': str(station)})
 
-valid_dutch_variables = ['WINDSPEED', 'RADIATION', 'TMAX', 'TMIN', 'TAVG']
+VALID_DUTCH_VAR_LIST = ['WINDSPEED', 'RADIATION', 'TMAX', 'TMIN', 'TAVG']
 
 VALID_DUTCH_VARIABLES = []
-for variable in valid_dutch_variables:
+for variable in VALID_DUTCH_VAR_LIST:
     VALID_DUTCH_VARIABLES.append({'label': variable, 'value': variable})
 
 #'German Station History', \
@@ -150,7 +150,5 @@ VALID_SCATTERPLOT_INTERVALS = [{'label': 'Daily', 'value': 'D'}, {'label': 'Week
 
 #Valid Histogram Bin Sizes
 VALID_HISTOGRAM_BINS = [{'label': 'Daily', 'value': 'D'}, {'label': 'Weekly', 'value': 'W'}, {'label': 'Monthly', 'value': 'M'}]
-
-
 
 VALID_DIFF_INTERVALS = [{'label': 'Last observation', 'value': 'Last observation'}, {'label': '1 year ago', 'value': '1 year ago'}]
