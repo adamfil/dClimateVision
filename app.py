73c02172f02ce15501bea037235634bfec573303
adam <82415896+adamfil@users.noreply.github.com>
2022-03-07 16:31:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from vars import VALID_DATASET_TYPES
 from vars import VALID_GRIDFILE_DASHSET
